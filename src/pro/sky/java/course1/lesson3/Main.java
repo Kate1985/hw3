c2b823ed4ad2d68a1634539eb2a0846bc54ef9b9
Kate <chupa-choops@bk.ru>
2021-11-04 22:25:49 +0300

    homework 3 is done

--- src/pro/sky/java/course1/lesson3/Main.java
+++ src/pro/sky/java/course1/lesson3/Main.java
@@ -2,7 +2,7 @@ package pro.sky.java.course1.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-
+//
         int clientOs = 1;
         if (clientOs == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
@@ -129,6 +129,6 @@ public class Main {
             System.out.println("Максимальный платеж при ЗП " + Salary + " равен " + maxPay + " рублей. Платеж по кредиту " + payCreditMonth + "рублей. Одобрено");
         }
 
-
+//
     }
 }
